167b5d6aaca5b2be9b5b4e7ce20193739e99fb89
Lawrence <locaulker@gmail.com>
2017-04-09 13:39:15 -0400

    browser-sync and gulp-watch added. Watching html and scss/css completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
 
 
 // Styles: SCSS to CSS
@@ -11,4 +13,31 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('./app/temp/css'))
 });
 
-gulp.task('default', ['styles']);
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/scss/**/*.scss', function() {
+    gulp.start('cssInject');
+  });
+
+});
+
+//CSS Inject by Browser
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/css/style.css')
+    .pipe(browserSync.stream());
+});
+
+
+gulp.task('default', ['watch']);
