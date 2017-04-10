0a1d4e9cda979df1fa151cf50a5f7570e320f37e
Lawrence <locaulker@gmail.com>
2017-04-10 17:08:20 -0400

    sourcemaps added. styles task completed

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,14 +2,15 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
-    plumber = require('gulp-plumber');
+    plumber = require('gulp-plumber'),
+    sourcemaps = require('gulp-sourcemaps');
 
 
 // Styles: SCSS to CSS
 gulp.task('styles', function() {
-  var args = {outputStyle: 'expanded'}
+  // For Development change outputStyle to expanded if necessary
+  var args = {outputStyle: 'compressed'}
   var args2 = [autoprefixer({browsers: ['last 2 versions']})];
-
 
   return gulp.src('./app/assets/scss/style.scss')
     .pipe(plumber(function(err) {
@@ -17,7 +18,9 @@ gulp.task('styles', function() {
       console.log(err);
       this.emit('end');
     }))
+    .pipe(sourcemaps.init())
     .pipe(sass(args))
     .pipe(postcss(args2))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./app/dist/css'))
 });
