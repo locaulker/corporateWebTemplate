4790dedc9a18ff3e13d2c9090f44b723c05a200c
Lawrence <locaulker@gmail.com>
2017-04-17 16:49:26 -0400

    Modified and Cleaned Workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,4 @@ require('./gulp/tasks/styles');
 require('./gulp/tasks/scripts');
 require('./gulp/tasks/images');
 require('./gulp/tasks/watch');
-// require('./gulp/tasks/build');
 require('./gulp/tasks/sprites');
