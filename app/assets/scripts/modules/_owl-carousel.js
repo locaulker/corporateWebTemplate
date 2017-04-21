cc3e9996dac4a5630024844ee82db8b6cd99ca6c
Lawrence <locaulker@gmail.com>
2017-04-21 13:20:01 -0400

    Completed Testimonials

--- app/assets/scripts/modules/_owl-carousel.js
+++ app/assets/scripts/modules/_owl-carousel.js
@@ -56,4 +56,23 @@ $(document).ready(function() {
 
   });
 
+  $("#owl-demo4").owlCarousel({
+
+     navigation : true, // Show next and prev buttons
+
+     slideSpeed : 300,
+     paginationSpeed : 400,
+
+     items : 1,
+     itemsDesktop : false,
+     itemsDesktopSmall : false,
+     itemsTablet: false,
+     itemsMobile : false,
+
+     autoPlay: false,
+     responsive: true
+
+ });
+
+
 });
