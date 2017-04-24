6622eaeea382d4ba8f06f47cf2c1232cd778a451
Lawrence <locaulker@gmail.com>
2017-04-24 10:50:52 -0400

    added animation, including progress bar

--- app/assets/scripts/modules/_main.js
+++ app/assets/scripts/modules/_main.js
@@ -1,6 +1,25 @@
 jQuery(document).ready(function(){
-  $('#menu').slicknav({
-    label: ''
+
+  /** -- Waypoints: Scroll to -- **/
+  $('.animation').each(function() {
+
+    var waypoint = new Waypoint({
+      element: this,
+      handler: function(direction) {
+        var animation = $(this.element).attr('data-animation');
+        $(this.element).css('opacity', '1');
+        $(this.element).addClass("animated " + animation);
+      },
+      offset: '30%'
+    });
 
   });
+
+  /** -- Smooth Scrolling -- **/
+  $('a').smoothScroll({
+    speed: 1000,
+    offset: -50
+  });
+
+
 });
